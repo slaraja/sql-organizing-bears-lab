0b545816b683d782524ad4089d41b03d08da02df
Stephanie La Raja <stephanielaraja@gmail.com>
2021-03-11 09:01:42 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,19 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 3, 'F', 'brown', 'angry', '0');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 5, 'M', 'brown', 'angry', '0');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 5, 'M', 'brown', 'angry', '0');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 5, 'M', 'brown', 'angry', '0');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 5, 'M', 'brown', 'angry', '0');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 5, 'M', 'brown', 'angry', '0');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 5, 'M', 'brown', 'angry', '0');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 5, 'M', 'brown', 'angry', '0');
+
+ /*
+Mr. Chocolate
+Rowdy
+Tabitha
+Sergeant Brown
+Melissa
+Grinch
+Wendy
+unnamed
+ */
